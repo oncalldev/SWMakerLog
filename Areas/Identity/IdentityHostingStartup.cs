8b19e24e6010325a1d650bdba81238f4f7a2c7fa
Bruce Grodnik <bgrodnik@oncalldev.com>
2020-01-30 21:25:31 -0600

    Source code inserted for Identity.

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SWMakerLog.Data;
+
+[assembly: HostingStartup(typeof(SWMakerLog.Areas.Identity.IdentityHostingStartup))]
+namespace SWMakerLog.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
